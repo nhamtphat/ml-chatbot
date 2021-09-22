e6e8b265946c66c9bbb8af041d1e144ca726ad10
Phat Nham <phattannham@gmail.com>
2021-09-22 14:31:07 +0700

    feat: chat script

---
+++ chat.py
@@ -0,0 +1,12 @@
+import reading
+
+def chat():
+	print("Start Talking with the bot(type quit to stop!")
+	while True:
+		inp = input("You: ")
+		if inp.lower() == "quit":
+			break
+
+		print(reading.response(inp))
+
+chat()
